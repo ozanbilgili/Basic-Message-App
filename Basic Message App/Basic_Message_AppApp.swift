ad4e4b0e944cf26f9f7cb183d9ec1b21b85e1f0d
ozanbilgili <ozan.bilgili@hotmail.com>
2021-10-26 20:42:32 +0300

    First Commit

---
+++ Basic Message App/Basic_Message_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Basic_Message_AppApp.swift
+//  Basic Message App
+//
+//  Created by Ozan Bilgili on 26.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct Basic_Message_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
