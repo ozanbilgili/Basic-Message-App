1d06f3e4daabb3857617381860696a57e6b66a9f
ozanbilgili <ozan.bilgili@hotmail.com>
2021-10-27 21:48:48 +0300

    UI feature

--- Basic Message App/LoginView.swift
+++ Basic Message App/LoginView.swift
@@ -8,18 +8,22 @@
 import SwiftUI
 import Firebase
 
-class FirebaseManager: NSObject{
+class FirebaseManager: NSObject {
     
     let auth: Auth
-
+    let storage: Storage
+    
     static let shared = FirebaseManager()
     
-    override init () {
+    override init() {
         FirebaseApp.configure()
+        
         self.auth = Auth.auth()
+        self.storage = Storage.storage()
         
         super.init()
     }
+    
 }
 
 struct LoginView: View {
@@ -28,30 +32,47 @@ struct LoginView: View {
     @State var email = ""
     @State var password = ""
     
+    @State var shouldShowImagePicker = false
+    
     var body: some View {
-        NavigationView{
-            ScrollView{
+        NavigationView {
+            ScrollView {
                 
-                VStack (spacing: 16){
-                    Picker(selection: $isLoginMode,  label:
-                            Text ("Picker here")){
-                    Text("Login")
+                VStack(spacing: 16) {
+                    Picker(selection: $isLoginMode, label: Text("Picker here")) {
+                        Text("Login")
                             .tag(true)
-                    Text("Create Account")
+                        Text("Create Account")
                             .tag(false)
-                    } .pickerStyle(SegmentedPickerStyle())
-                       
+                    }.pickerStyle(SegmentedPickerStyle())
+                        
                     if !isLoginMode {
                         Button {
-                            
+                            shouldShowImagePicker.toggle()
                         } label: {
-                            Image(systemName: "person.fill")
-                                .font(.system(size: 64))
-                                .padding()
+                            
+                            VStack {
+                                if let image = self.image {
+                                    Image(uiImage: image)
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 128, height: 128)
+                                        .cornerRadius(64)
+                                } else {
+                                    Image(systemName: "person.fill")
+                                        .font(.system(size: 64))
+                                        .padding()
+                                        .foregroundColor(Color(.label))
+                                }
+                            }
+                            .overlay(RoundedRectangle(cornerRadius: 64)
+                                        .stroke(Color.black, lineWidth: 3)
+                            )
+                            
                         }
                     }
-                   
-                    Group{
+                    
+                    Group {
                         TextField("Email", text: $email)
                             .keyboardType(.emailAddress)
                             .autocapitalization(.none)
@@ -59,75 +80,109 @@ struct LoginView: View {
                     }
                     .padding(12)
                     .background(Color.white)
-
+                    
                     Button {
                         handleAction()
                     } label: {
-                        HStack{
+                        HStack {
                             Spacer()
                             Text(isLoginMode ? "Log In" : "Create Account")
                                 .foregroundColor(.white)
                                 .padding(.vertical, 10)
                                 .font(.system(size: 14, weight: .semibold))
-                                
                             Spacer()
-                        } .background(Color.blue)
+                        }.background(Color.blue)
                         
                     }
+                    
                     Text(self.loginStatusMessage)
                         .foregroundColor(.red)
                 }
                 .padding()
+                
             }
-            
             .navigationTitle(isLoginMode ? "Log In" : "Create Account")
             .background(Color(.init(white: 0, alpha: 0.05))
                             .ignoresSafeArea())
-            }
+        }
         .navigationViewStyle(StackNavigationViewStyle())
-        }
-    private func handleAction() {
-        if isLoginMode {
-           // print("Should log into Firebase with existing credentials")
-            loginUser()
-        }
-        else {
-            createNewAccount()
-           // print("Register a new account")
+        .fullScreenCover(isPresented: $shouldShowImagePicker, onDismiss: nil) {
+            ImagePicker(image: $image)
         }
     }
-    @State var loginStatusMessage = ""
+    
+    @State var image: UIImage?
+    
+    private func handleAction() {
+        if isLoginMode {
+
+            loginUser()
+        } else {
+            createNewAccount()
+
+        }
+    }
+    
     private func loginUser() {
-        FirebaseManager.shared.auth.signIn(withEmail: email, password: password) {
-            result, err in
+        FirebaseManager.shared.auth.signIn(withEmail: email, password: password) { result, err in
             if let err = err {
                 print("Failed to login user:", err)
                 self.loginStatusMessage = "Failed to login user: \(err)"
                 return
             }
-            print ("Succesfully logged in as user: \(result?.user.uid ?? "")")
-            self.loginStatusMessage = "Succesfully logged in as user: \(result?.user.uid ?? "")"
+            
+            print("Successfully logged in as user: \(result?.user.uid ?? "")")
+            
+            self.loginStatusMessage = "Successfully logged in as user: \(result?.user.uid ?? "")"
         }
     }
-        
-    private func createNewAccount(){
-        FirebaseManager.shared.auth.createUser(withEmail: email, password: password) {
-            result, err in
+    
+    @State var loginStatusMessage = ""
+    
+    private func createNewAccount() {
+        FirebaseManager.shared.auth.createUser(withEmail: email, password: password) { result, err in
             if let err = err {
                 print("Failed to create user:", err)
                 self.loginStatusMessage = "Failed to create user: \(err)"
                 return
             }
-            print ("Succesfully created user: \(result?.user.uid ?? "")")
-            self.loginStatusMessage = "Succesfully created user: \(result?.user.uid ?? "")"
+            
+            print("Successfully created user: \(result?.user.uid ?? "")")
+            
+            self.loginStatusMessage = "Successfully created user: \(result?.user.uid ?? "")"
+            
+            self.persistImageToStorage()
+        }
+    }
+    
+    private func persistImageToStorage() {
+//        let filename = UUID().uuidString
+        guard let uid = FirebaseManager.shared.auth.currentUser?.uid
+            else { return }
+        let ref = FirebaseManager.shared.storage.reference(withPath: uid)
+        guard let imageData = self.image?.jpegData(compressionQuality:
+            0.5) else { return }
+        ref.putData(imageData, metadata: nil) { metadata, err in
+            if let err = err {
+                self.loginStatusMessage = "Failed to push image to Storage: \(err)"
+                return
+            }
+            
+            ref.downloadURL { url, err in
+                if let err = err {
+                    self.loginStatusMessage = "Failed to retrieve downloadURL: \(err)"
+                    return
+                }
+                
+                self.loginStatusMessage = "Successfully stored image with url: \(url?.absoluteString ?? "")"
+                print(url?.absoluteString)
+            }
         }
     }
 }
-    
 
 struct ContentView_Previews1: PreviewProvider {
     static var previews: some View {
         LoginView()
     }
 }
-
